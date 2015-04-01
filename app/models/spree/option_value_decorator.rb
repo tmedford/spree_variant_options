f87da7e5edb116658a4a63e9e4f23116d9de44b1
Ian Young <ian@iangreenleaf.com>
2015-03-31 22:16:47 -0700

    Add `small` size to thumbnails
    This is the size that's actually used when generating the options dialog.

--- app/models/spree/option_value_decorator.rb
+++ app/models/spree/option_value_decorator.rb
@@ -1,7 +1,7 @@
 Spree::OptionValue.class_eval do
 
   has_attached_file :image,
-    :styles        => { mini: '32x32>', normal: '128x128>' },
+    :styles        => { mini: '32x32>', small: '64x64>', normal: '128x128>' },
     :default_style => SpreeVariantOptions::VariantConfig[:option_value_default_style],
     :url           => SpreeVariantOptions::VariantConfig[:option_value_url],
     :path          => SpreeVariantOptions::VariantConfig[:option_value_path]
