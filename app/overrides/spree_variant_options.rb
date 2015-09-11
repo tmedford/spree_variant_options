e92bac041e4a6e9f55f5ec568490ee93e4385b6a
Taylor Medford <taylor.lee.medford@gmail.com>
2015-09-11 00:51:19 -0400

    Remove overides

--- app/overrides/spree_variant_options.rb
+++ app/overrides/spree_variant_options.rb
@@ -8,10 +8,10 @@ Deface::Override.new(:virtual_path   => "spree/admin/option_types/edit",
                      :name           => "admin_option_value_table_headers",
                      :replace        => "thead[data-hook=option_header]",
                      :partial        => "spree/admin/option_values/table_header",
-                     :disabled       => false)
+                     :disabled       => true)
 
 Deface::Override.new(:virtual_path   => "spree/admin/option_types/edit",
                      :name           => "admin_option_value_table_empty_colspan",
                      :set_attributes => "tr[data-hook=option_none] td",
                      :attributes     => { :colspan => 5 },
-                     :disabled       => false)
+                     :disabled       => true)
