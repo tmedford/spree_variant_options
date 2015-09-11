f48568adf0ec59dbf797f88324861b9524b3df2a
Taylor Medford <taylor.lee.medford@gmail.com>
2015-09-11 01:11:09 -0400

    test

--- app/overrides/spree_variant_options.rb
+++ app/overrides/spree_variant_options.rb
@@ -3,15 +3,3 @@ Deface::Override.new(:virtual_path   => "spree/products/_cart_form",
                      :replace        => "#product-variants",
                      :partial        => "spree/products/variant_options",
                      :disabled       => false)
-
-Deface::Override.new(:virtual_path   => "spree/admin/option_types/edit",
-                     :name           => "admin_option_value_table_headers",
-                     :replace        => "thead[data-hook=option_header]",
-                     :partial        => "spree/admin/option_values/table_header",
-                     :disabled       => true)
-
-Deface::Override.new(:virtual_path   => "spree/admin/option_types/edit",
-                     :name           => "admin_option_value_table_empty_colspan",
-                     :set_attributes => "tr[data-hook=option_none] td",
-                     :attributes     => { :colspan => 5 },
-                     :disabled       => true)
